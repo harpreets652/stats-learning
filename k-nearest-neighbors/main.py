01e08a985e6bb7b3294e66a47115d409efb7e989
harpreetsingh <harpreets@nevada.unr.edu>
2018-01-31 09:32:51 -0800

    classifier and test data

---
+++ k-nearest-neighbors/main.py
@@ -0,0 +1,17 @@
+import os
+import lib.knn_classifier as knn
+
+classifier = knn.KnnClassifier(
+    [0, "/Users/harpreetsingh/github/stats-learning/k-nearest-neighbors/resources/group_1/train0.txt"],
+    [3, "/Users/harpreetsingh/github/stats-learning/k-nearest-neighbors/resources/group_1/train3.txt"])
+
+
+# read test data
+
+
+# loop over k from 1..15, odd values
+# loop over test data
+# test classification for the classes in question
+# tally metrics
+# print metrics
+

e30e9bef91165964d974b215e30d5f30b124f851
harpreetsingh <harpreets@nevada.unr.edu>
2018-05-13 11:28:44 -0700

    get deep features

--- random-forest/main.py
+++ random-forest/main.py
@@ -3,6 +3,7 @@ from keras import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 from feature_providers import bag_of_features as bof
+from feature_providers import cifar_cnn as cnn
 
 
 def main():
@@ -12,6 +13,11 @@ def main():
     img_feature_gen = bof.BagOfFeaturesTransform(patch_size=16, num_clusters=256)
     training_x = img_feature_gen.initialize(x_train)
     print("Image features extracted")
+
+    # deep features
+    # cnn_model_path = "/Users/harpreetsingh/github/stats-learning/random-forest/results/cnn_results_2_best/cifar_10_model.h5"
+    # model = cnn.load_model(cnn_model_path)
+    # training_x = cnn.get_deep_features_set(model, x_train)
 
     classifier = RandomForestClassifier(n_estimators=10, criterion="gini", min_samples_split=2, n_jobs=-1)
     classifier.fit(training_x, np.ravel(y_train))
